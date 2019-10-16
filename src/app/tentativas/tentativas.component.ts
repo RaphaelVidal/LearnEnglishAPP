07622806ce5046b664490f652316efbdc359890d
Raphael Vidal <raphael.r_vidal@hotmail.com>
2019-10-16 20:33:02 -0300

    property binding

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentativasComponent implements OnInit {
 
+public coracaoCheio:String = '/assets/coracao_cheio.png'
+public coracaoVazio:String = '/assets/coracao_vazio.png'
+
   constructor() { }
 
   ngOnInit() {
