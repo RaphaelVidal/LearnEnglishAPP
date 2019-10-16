0d91e17a5b44ff5b1bb3203e12a43abce99afbe3
Raphael Vidal <raphael.r_vidal@hotmail.com>
2019-10-16 20:56:58 -0300

    definição de classe menos verbosa

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,10 @@
+export class Frase {
+    // public fraseEngl: string
+    // public frasePtBr: string
+
+    // constructor(fraseEngl: string, frasePtBr: string) {
+    //     this.fraseEngl = fraseEngl
+    //     this.frasePtBr = frasePtBr
+    // }
+    constructor(public fraseEngl: string, public frasePtBr: string) {  }
+}
