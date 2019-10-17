a90a997bb2a3cd6970afacaca02de0be2afec9ce
Raphael Vidal <raphael.r_vidal@hotmail.com>
2019-10-17 20:02:04 -0300

    criação moc

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from "../shared/frase.model";
+
+const FRASES: Frase[] = [
+    {fraseEngl:'I like to learn',frasePtBr: 'Eu gosto de aprender'},
+    {fraseEngl:'I watch tv',frasePtBr: 'Eu assito tv'},
+    {fraseEngl:'How are you?',frasePtBr: 'Como vai vc?'},
+    {fraseEngl:'I eat bread',frasePtBr: 'Eu como pão'}
+]
